f4735991fd6ab0a0f857ff764e19331f6ef2f98b
Akosua <aaasante@uwaterloo.ca>
2016-02-15 19:52:27 -0500

    Added time delay

--- question generator.py
+++ question generator.py
@@ -1,4 +1,15 @@
 import pymysql
+import msvcrt
+import time
+
+def _question_gen(packet_row):
+	question = packet_row[u'Questions']
+	question_words = question.split(' ')
+	return question_words
+
+def _answer_gen(packet_row):
+	answer = packet_row[u'Answers']
+	return answer
 
 # initiates variables for pymysql connection
 connection = pymysql.connect(host="localhost",
@@ -15,15 +26,19 @@ cursor.execute(sql_command)
 results = cursor.fetchall()
 
 
+
 while True:
         req_input = raw_input("Type 'n' for next question or 'q' to exit") #work on making this just a keypress especially for two player game later on
-        if req_input == 'n':
+        if req_input == 'n':			
                 for row in results:
-                        question = row[u'Questions']
-                        answer = row[u'Answers']
-                        print(question)
-                        raw_input('>>>')
-                        print(answer)
+                        curr_question = _question_gen(row)
+                        curr_answer = _answer_gen(row)
+                        for word in curr_question:
+                                print word,
+                                time.sleep(0.2)
+                                if msvcrt.kbhit():
+                                        raw_input("Type an answer")
+						
                         cont_input = raw_input("Type 'n' for next question or 'q' to exit")
                         if cont_input == 'n':
                                 pass
@@ -39,7 +54,7 @@ while True:
 #closes database connection
 connection.close()
 
-#refactor into functions
+#refactor into functions; use testing question display to work out how to print questions; print answers normally
 #make a gui for this
 #make a regex checker for anwers
 #make it show up word by word
